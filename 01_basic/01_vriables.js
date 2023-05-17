0c08bc6e458489a20d09e776a936abc653170231
msahil9833 <86947345+msahil9833@users.noreply.github.com>
2023-05-17 11:32:01 +0000

    datatypes done

---
+++ 01_basic/01_vriables.js
@@ -0,0 +1,14 @@
+const rollnumber = "22ccixx660"
+//this value cant be chnaged in whole program
+//rollnumber = "22ccixxx660"
+let username = "MOHD SAHIL"
+//this value and verable name can be same so we use let 
+//incase if we have to change then we can do that 
+//name = "MOHD SAHIL"
+var branch = "MCA"
+//this varible has issues in scope so when early time devloper
+//use this then same name can be make confit 
+//that why nowdays we avoid the var 
+//branch = "MCA"
+console.log(rollnumber);
+console.table([rollnumber, username, branch]);
---
+++ 01_basic/02.datatypes.js
@@ -0,0 +1,9 @@
+"use strict "; //treat all js code as new verrsion
+//alert(3+3)
+//we are using nodejs not a browser console
+console.log(3+3); 
+console.log("MOHD SAHLI");
+//right way to write code for readability
+console.log(3 + 
+    3);
+//output will be same but not readable .
